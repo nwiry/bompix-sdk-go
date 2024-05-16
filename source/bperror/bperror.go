d1a7fab7ba6cad5884842ca5fe30f728bfdfd40e
Diogo Cerqueira <contato@diogocerqueira.dev.br>
2024-05-16 01:09:49 -0300

    Added error response error struct

---
+++ source/bperror/bperror.go
@@ -0,0 +1,6 @@
+package bperror
+
+// ResponseError representa um erro retornado pela API Bompix.
+type ResponseError struct {
+	Message string // Message é a mensagem de erro retornada pela API.
+}
